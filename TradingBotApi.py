9f02f62ff89659a203d7dd8a96e24996edee0a71
DarkMagician <duy.nguyen2202087@std.ttu.edu.vn>
2025-01-05 22:37:16 +0700

    Complete!

--- TradingBotApi.py
+++ TradingBotApi.py
@@ -1,16 +1,41 @@
-from fastapi import FastAPI
+import base64
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, validator
+from diffusers import DiffusionPipeline
+import torch
+from io import BytesIO
+import uvicorn
 import json
 from pathlib import Path
 
+
 app = FastAPI()
+
+# Allowed origins
+origins = [
+    "http://localhost:3000",
+    "https://memetrade-co.fun",
+    "https://www.memetrade-co.fun"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # File paths to the JSON files
 json_files = [
-    'Currencies\Output\BTCUSDT.json',
-    'Currencies\Output\ETHUSDT.json',
-    'Currencies\Output\BNBUSDT.json',
-    'Currencies\Output\ADAUSDT.json',
-    'Currencies\Output\SOLUSDT.json'
+    'Currencies/Output/BTCUSDT.json',
+    'Currencies/Output/ETHUSDT.json',
+    'Currencies/Output/BNBUSDT.json',
+    'Currencies/Output/ADAUSDT.json',
+    'Currencies/Output/SOLUSDT.json'
 ]
 
 
@@ -31,6 +56,6 @@ async def read_all_json():
             currency = Path(file_path).stem  # Extract the file name without extension (e.g., BTCUSDT)
             all_data[currency] = data
         except Exception as e:
-            all_data[currency] = {"error": str(e)}
+            return {"error": str(e)}
 
     return {"files_data": all_data}
